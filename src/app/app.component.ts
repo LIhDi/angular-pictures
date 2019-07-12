55be24b7e6d2dea041dcad60f50f3be08b4718f7
Aline Diogo <alinecsdiogo@gmail.com>
2019-07-12 20:55:49 +0000

    componente foto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  photos = [
+    {
+      url: 'https://cifrasparaflauta.files.wordpress.com/2009/07/uzumaki-naruto1.png',
+      description: 'Leão'
+    },
+    {
+      url: 'https://cde.peru.com//ima/0/1/1/0/6/1106194/611x458/naruto.jpg',
+      description: 'Leoa'
+    },
+    {
+      url: 'https://www.einerd.com.br/wp-content/uploads/2017/10/Naruto_Uzumaki.jpg',
+      description: 'Gata'
+    },
+    {
+      url: 'https://cifrasparaflauta.files.wordpress.com/2009/07/uzumaki-naruto1.png',
+      description: 'Gata'
+    }
+  ];
 }
