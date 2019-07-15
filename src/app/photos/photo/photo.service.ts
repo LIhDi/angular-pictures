b9bd7b8268e1aca1ada0deccf694f53f5fae599a
Aline Diogo <alinecsdiogo@gmail.com>
2019-07-15 21:15:07 +0000

    rotas

--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Photo } from './photo';
 
+const API = 'https://3000-e7a1f90e-d9da-491c-8ff0-08391352c19c.ws-us0.gitpod.io/';
+
 @Injectable( { providedIn: 'root'} )
 export class PhotoService {
 
@@ -9,7 +11,7 @@ export class PhotoService {
 
   listFromUser(userName: string) {
     return this.http
-    .get<Photo[]>('https://3000-e7a1f90e-d9da-491c-8ff0-08391352c19c.ws-us0.gitpod.io/flavio/photos')
+    .get<Photo[]>(API + userName + '/photos')
 
   }
 
