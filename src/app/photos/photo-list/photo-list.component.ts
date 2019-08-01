b2b77de102363ec2891bed9a79522fde5b4bd7af
Aline Diogo <alinecsdiogo@gmail.com>
2019-08-01 20:56:19 +0000

    binding de eventos

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -10,7 +10,8 @@ import { Photo } from '../photo/photo';
 })
 export class PhotoListComponent implements OnInit {
 
-  photos : Photo[] = [];
+  photos: Photo[] = [];
+  filter: string = '';
 
 constructor(
     private photoService: PhotoService,
